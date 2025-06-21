f69e5afd4394cf6ed0be9efb9830970e63a9ce23
unknown <druzzzsk@yandex.ru>
2025-06-21 17:01:41 +0300

    first commit

---
+++ utils/__init__.py
@@ -0,0 +1,2 @@
+from utils.feature_engineering_tools import get_full_descriptors_set, get_full_descriptors_for_only_seq_classifier 
+from utils.constants import sel_features
---
+++ utils/autoencoder/__init__.py
@@ -0,0 +1,6 @@
+from utils.autoencoder.Funcs import (
+    encoding,
+    generate_latent_representations,
+    filter_sequences,
+    generate_rdkit_descriptors
+)
